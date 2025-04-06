e48fe8e70cec3f1e04f318a79498a7a6667db7f0
Sofiia Syuy <sofiya.syuy@mail.ru>
2025-04-06 14:46:08 +0300

    hmw3

--- base_service/src/usecase/grpc.py
+++
@@ -1,6 +0,0 @@
-import grpc
-from base_service.src.proto.posts_service_pb2_grpc import PostServiceStub
-
-async def grpc_connect():
-    channel = grpc.insecure_channel("post_service:80")
-    return PostServiceStub(channel)
